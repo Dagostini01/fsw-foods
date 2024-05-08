9062f047d183a5d76fb5c1c275fadf9c8d4d50c5
Dagostini01 <pedro01dagostini@gmail.com>
2024-05-08 15:11:52 -0300

    feat: add search inital component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Header from "./components/ui/header";
+import Search from "./components/ui/search";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 }
