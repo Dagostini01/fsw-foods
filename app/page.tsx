03da6e9d5a2da47a5d66944c4e3e9efffe00d9d3
Dagostini01 <pedro01dagostini@gmail.com>
2024-05-14 12:51:07 -0300

    feat: add restaurant list components

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Search from "./components/ui/search";
 import Image from "next/image";
 import { db } from "./lib/prisma";
 import PromoBanner from "./components/ui/promo-banner";
+import RestaurantList from "./components/ui/restaurant-list";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -57,6 +58,18 @@ const Home = async () => {
           src={"/promo-banner02.png"}
           alt="A partir de R$17,90 em lanches"
         />
+      </div>
+      <div className="space-y-1 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos <ChevronRightIcon size={16} />{" "}
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </>
   );
